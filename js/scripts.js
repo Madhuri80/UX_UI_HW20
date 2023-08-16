172d2dc30a84e3f8f9011ce8bdde139ca76e257c
Madhuri80 <127690787+Madhuri80@users.noreply.github.com>
2023-08-16 15:36:17 -0500

    h

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+console.log ("External JS Linked");
+ function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
